a16c9154ca5998801a2027e9ae6522702cdcdf23
Alexandr Konukhov <jesusmisha@gmail.com>
2023-04-25 15:07:04 +0300

    added part for waiting page to load

--- 2_started_work_with_playwright/2_1_writing_scripts_with_codegen/main.py
+++ 2_started_work_with_playwright/2_1_writing_scripts_with_codegen/main.py
@@ -5,7 +5,7 @@ def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://playwright-todomvc.antonzimaiev.repl.co/#/")
+    page.goto("https://playwright-todomvc.antonzimaiev.repl.co/#/", wait_until='domcontentloaded')
     page.get_by_placeholder("What needs to be done?").click()
     page.get_by_placeholder("What needs to be done?").fill("sssss")
     page.get_by_placeholder("What needs to be done?").press("Enter")
