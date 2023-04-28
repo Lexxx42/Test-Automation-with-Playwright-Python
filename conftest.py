e2888027f5a7273b5ca13bff52066924b8286628
Alexandr Konukhov <jesusmisha@gmail.com>
2023-04-28 15:14:54 +0300

    added conftest.py to the project root

---
+++ conftest.py
@@ -0,0 +1,35 @@
+import pytest
+from playwright.sync_api import Playwright, BrowserType
+
+
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {
+        "viewport": {
+            "width": 1920,
+            "height": 1080
+        }
+    }
+
+
+@pytest.fixture(scope="session")
+def browser_type(playwright: Playwright):
+    return playwright.chromium
+
+
+@pytest.fixture(scope="session")
+def browser_type_launch_args():
+    return {
+        "headless": False
+    }
+
+
+@pytest.fixture(scope="session")
+def context(browser_type: BrowserType, browser_type_launch_args, browser_context_args):
+    context = browser_type.launch_persistent_context("./foobar", **{
+        **browser_type_launch_args,
+        **browser_context_args,
+        "locale": "en-GB",
+    })
+    yield context
+    context.close()
